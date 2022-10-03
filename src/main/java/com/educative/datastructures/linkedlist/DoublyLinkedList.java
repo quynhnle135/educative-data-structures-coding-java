03f5b6a3e5381bd77ebdc21d4a7d5fb7309aa9a3
Quynh <quynhnle135@gmail.com>
2022-10-03 08:05:29 -0400

    learned how to delete with doubly linkedlist

--- src/main/java/com/educative/datastructures/linkedlist/DoublyLinkedList.java
+++ src/main/java/com/educative/datastructures/linkedlist/DoublyLinkedList.java
@@ -32,4 +32,57 @@ public class DoublyLinkedList<T> {
         this.headNode = newNode;
         size++;
     }
+
+    public void printList() {
+        if (isEmpty()) {
+            System.out.println("List is empty");
+            return;
+        }
+        Node temp = headNode;
+        System.out.print("<- null ");
+        while (temp.nextNode != null) {
+            System.out.print(temp.data.toString() + " <-> ");
+            temp = temp.nextNode;
+        }
+        System.out.println(temp.data.toString() + " -> null");
+
+    }
+
+    public void deleteAtHead() {
+        if (isEmpty()) {
+            return;
+        }
+        headNode = headNode.nextNode;
+        headNode.prevNode = null;
+        size--;
+    }
+
+    public void deleteByValue(T data) {
+        if (isEmpty()) {
+            return;
+        }
+        Node currentNode = this.headNode;
+        if (currentNode.data.equals(data)) {
+            deleteAtHead();
+            return;
+        }
+        while (currentNode != null) {
+            if (data.equals(currentNode.data)) {
+                currentNode.prevNode.nextNode = currentNode.nextNode;
+                if (currentNode.nextNode != null) {
+                    currentNode.nextNode.prevNode = currentNode.prevNode;
+                }
+                size--;
+            }
+            currentNode = currentNode.nextNode;
+        }
+    }
+
+    public static void main(String[] args) {
+        DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
+        for (int i = 1; i <= 5; i++) {
+            list.insertAtHead(i);
+        }
+        list.printList();
+    }
 }
