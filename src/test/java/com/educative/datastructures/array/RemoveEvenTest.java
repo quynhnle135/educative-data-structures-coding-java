05403df11a677af776ca2f0f4b8de7501bd03572
Quynh <quynhnle135@gmail.com>
2022-10-08 09:33:17 -0400

    Write tests for removeEvenUsingList

--- src/test/java/com/educative/datastructures/array/RemoveEvenTest.java
+++ src/test/java/com/educative/datastructures/array/RemoveEvenTest.java
@@ -3,7 +3,9 @@ package com.educative.datastructures.array;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class RemoveEvenTest {
     @Test
@@ -24,9 +26,37 @@ public class RemoveEvenTest {
 
     @Test
     public void testRemoveEvenEmpty() {
-        int[] arr= {};
+        int[] arr = {};
         int[] result = RemoveEven.removeEven(arr);
         int[] expectedResult = {};
         Assertions.assertArrayEquals(expectedResult, result);
     }
+
+    @Test
+    public void testRemoveEvenUsingList() {
+        int[] array = {1, 2, 3, 4, 5};
+        List<Integer> actual = RemoveEven.removeEvenUsingList(array);
+        List<Integer> expected = new ArrayList<>();
+        expected.addAll(Arrays.asList(1, 3, 5));
+        Assertions.assertTrue(expected.equals(actual));
+    }
+
+    @Test
+    public void testRemoveEvenUsingListNegative() {
+        int[] array = {-1, -2, -3, -4, -5};
+        List<Integer> actual = RemoveEven.removeEvenUsingList(array);
+        List<Integer> expected = new ArrayList<>();
+        expected.addAll(Arrays.asList(-1, -3, -5));
+        Assertions.assertTrue(expected.equals(actual));
+    }
+
+    @Test
+    public void testRemoveEvenUsingListEmpty() {
+        int[] arr = {};
+        List<Integer> actual = RemoveEven.removeEvenUsingList(arr);
+        List<Integer> expected = new ArrayList<>();
+        expected.addAll(Arrays.asList());
+        Assertions.assertTrue(expected.equals(actual));
+    }
+
 }
