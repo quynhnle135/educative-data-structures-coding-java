5be379a12a645c4d81b5c1936f41868419b7ed35
Quynh <quynhnle135@gmail.com>
2022-10-08 09:48:44 -0400

    Review Merge Two Array exercise

--- src/main/java/com/educative/datastructures/array/MergeTwoArray.java
+++ src/main/java/com/educative/datastructures/array/MergeTwoArray.java
@@ -19,4 +19,41 @@ public class MergeTwoArray {
         }
         return res;
     }
+
+    static int[] merge(int[] arr1, int[] arr2) {
+        int[] res = new int[arr1.length + arr2.length];
+        int i = 0, j = 0, k = 0;
+        while (i < arr1.length && j < arr2.length) {
+            if (arr1[i] < arr2[j]) {
+                res[k++] = arr1[i++];
+            } else {
+                res[k++] = arr2[j++];
+            }
+        }
+        while (i < arr1.length) {
+            res[k++] = arr1[i++];
+        }
+        while (j < arr2.length) {
+            res[k++] = arr2[j++];
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4};
+        int[] arr2 = {5, 6, 7, 8};
+        int[] res1 = mergeArray(arr1, arr2);
+        int[] res2 = merge(arr1, arr2);
+        System.out.println("Res 1: ");
+        for (int n : res1) {
+            System.out.print(n + " ");
+        }
+        System.out.println();
+        System.out.println("Res 2: ");
+        for (int n : res2) {
+            System.out.print(n + " ");
+        }
+
+
+    }
 }
--- src/test/java/com/educative/datastructures/array/MergeTwoArrayTest.java
+++ src/test/java/com/educative/datastructures/array/MergeTwoArrayTest.java
@@ -48,4 +48,49 @@ public class MergeTwoArrayTest {
         int[] expectedResult = {-99, -98, 1, 2, 3, 4, 5, 97};
         Assertions.assertArrayEquals(expectedResult, result);
     }
+
+    @Test
+    public void testMergeArr() {
+        int[] arr1 = {1, 3, 5};
+        int[] arr2 = {2, 4, 6};
+        int[] actual = MergeTwoArray.merge(arr1, arr2);
+        int[] expected = {1, 2, 3, 4, 5, 6};
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testMergeArrNegative() {
+        int[] arr1 = {-5, -3, -1};
+        int[] arr2 = {-6, -4, -2};
+        int[] result = MergeTwoArray.merge(arr1, arr2);
+        int[] expectedResult = {-6, -5, -4, -3, -2, -1};
+        Assertions.assertArrayEquals(expectedResult, result);
+    }
+
+    @Test
+    public void testMergeArrEmpty() {
+        int[] arr1 = {};
+        int[] arr2 = {};
+        int[] result = MergeTwoArray.merge(arr1, arr2);
+        int[] expectedResult = {};
+        Assertions.assertArrayEquals(expectedResult, result);
+    }
+
+    @Test
+    public void testMergeArrEmptyMixed() {
+        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] arr2 = {-99, -98, -97};
+        int[] result = MergeTwoArray.merge(arr1, arr2);
+        int[] expectedResult = {-99, -98, -97, 1, 2, 3, 4, 5};
+        Assertions.assertArrayEquals(expectedResult, result);
+    }
+
+    @Test
+    public void testMergeArrEmptyMixedA() {
+        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] arr2 = {-99, -98, 97};
+        int[] result = MergeTwoArray.mergeArray(arr1, arr2);
+        int[] expectedResult = {-99, -98, 1, 2, 3, 4, 5, 97};
+        Assertions.assertArrayEquals(expectedResult, result);
+    }
 }
