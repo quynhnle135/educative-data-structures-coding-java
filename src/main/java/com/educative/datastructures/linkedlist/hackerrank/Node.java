ce7afc1bf049ad55141f2d1051510c2054273d6e
Quynh <quynhnle135@gmail.com>
2022-10-09 09:28:15 -0400

    Reverse Linkedlist

--- src/main/java/com/educative/datastructures/linkedlist/hackerrank/Node.java
+++ src/main/java/com/educative/datastructures/linkedlist/hackerrank/Node.java
@@ -1,8 +1,8 @@
 package com.educative.datastructures.linkedlist.hackerrank;
 
 public class Node {
-    Node next;
-    int data;
+    public Node next;
+    public int data;
 
     public Node() {
 
@@ -17,14 +17,6 @@ public class Node {
         this.next = next;
     }
 
-    public void append(int data) {
-        Node current = this;
-        while (current.next != null) {
-            current = current.next;
-        }
-        current.next = new Node(data);
-    }
-
     @Override
     public String toString() {
         return "Node{" +
@@ -33,4 +25,39 @@ public class Node {
                 '}';
     }
 
+    public static void traverse(Node headNode) {
+        if (headNode == null) return;
+        while (headNode != null) {
+            System.out.print(headNode.data + " -> ");
+            headNode = headNode.next;
+        }
+        System.out.print("null");
+        System.out.println();
+    }
+
+    public static Node reverse(Node headNode) {
+        Node cur = headNode;
+        Node prev = null;
+        while (cur != null) {
+            Node nextTemp = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = nextTemp;
+        }
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        Node node5 = new Node(5);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        traverse(node1);
+        traverse(reverse(node1));
+    }
 }
