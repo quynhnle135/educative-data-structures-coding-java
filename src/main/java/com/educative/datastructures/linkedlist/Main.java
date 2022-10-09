f0718721d558f928ad0f93ae1544df8100b14abd
Quynh <quynhnle135@gmail.com>
2022-10-09 19:39:12 -0400

    Singly LinkedList implementation review

--- src/main/java/com/educative/datastructures/linkedlist/Main.java
+++ src/main/java/com/educative/datastructures/linkedlist/Main.java
@@ -1,4 +1,12 @@
 package com.educative.datastructures.linkedlist;
 
+
 public class Main {
+    public static void main(String[] args) {
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+        for (int i = 1; i <= 10; i++) {
+            list.insertAtHead(i);
+        }
+        list.printList();
+    }
 }
--- src/main/java/com/educative/datastructures/linkedlist/SinglyLinkedList.java
+++ src/main/java/com/educative/datastructures/linkedlist/SinglyLinkedList.java
@@ -19,10 +19,18 @@ public class SinglyLinkedList<T> {
     }
 
     public boolean isEmpty() {
-        if (headNode == null) {
-            return true;
-        }
+        if (headNode == null) return true;
         return false;
+    }
+
+    public void printList() {
+        Node cur = headNode;
+        while (cur != null) {
+            System.out.print(cur.data + " -> ");
+            cur = cur.nextNode;
+        }
+        System.out.print("null");
+        System.out.println();
     }
 
     public void insertAtHead(T data) {
@@ -41,8 +49,8 @@ public class SinglyLinkedList<T> {
         Node newNode = new Node();
         newNode.data = data;
         newNode.nextNode = null;
-        Node last = headNode;
 
+        Node last = headNode;
         while (last.nextNode != null) {
             last = last.nextNode;
         }
@@ -53,24 +61,22 @@ public class SinglyLinkedList<T> {
     public void insertAfter(T data, T previous) {
         Node newNode = new Node();
         newNode.data = data;
-        Node currentNode = this.headNode;
-        while (currentNode != null && !currentNode.data.equals(previous)) {
-            currentNode = currentNode.nextNode;
+        Node cur = headNode;
+        while (!cur.data.equals(previous)) {
+            cur = cur.nextNode;
         }
-        if (currentNode != null) {
-            newNode.nextNode = currentNode.nextNode;
-            currentNode.nextNode = newNode;
+        if (cur != null) {
+            newNode.nextNode = cur.nextNode;
+            cur.nextNode = newNode;
             size++;
         }
     }
 
-    public boolean searchNode(T data) {
-        Node currentNode = headNode;
-        while (currentNode != null) {
-            if (currentNode.data.equals(data)) {
-                return true;
-            }
-            currentNode = currentNode.nextNode;
+    public boolean search(T data) {
+        Node cur = headNode;
+        while (cur != null) {
+            if (cur.data.equals(data)) return true;
+            cur = cur.nextNode;
         }
         return false;
     }
@@ -83,103 +89,26 @@ public class SinglyLinkedList<T> {
         size--;
     }
 
-    public void deleteAtEnd() {
-        if (isEmpty()) {
-            return;
-        }
-        Node prevNode = this.headNode;
-        Node currentNode = prevNode.nextNode;
-        while (currentNode.nextNode != null) {
-            prevNode = currentNode;
-            currentNode = currentNode.nextNode;
-        }
-        prevNode.nextNode = null;
-        size--;
-    }
-
     public void deleteByValue(T data) {
         if (isEmpty()) {
             return;
         }
-        Node currentNode = this.headNode;
-        Node prevNode = null;
-        if (currentNode.data.equals(data)) {
+
+        Node cur = headNode;
+        Node prev = null;
+
+        if (cur.data.equals(data)) {
             deleteAtHead();
             return;
         }
-        while (currentNode != null) {
-            if (currentNode.data.equals(data)) {
-                prevNode.nextNode = currentNode.nextNode;
+        while (cur != null) {
+            if (cur.data.equals(data)) {
+                prev.nextNode = cur.nextNode;
                 size--;
                 return;
             }
-            prevNode = currentNode;
-            currentNode = currentNode.nextNode;
+            prev = cur;
+            cur = cur.nextNode;
         }
-    }
-
-    public void printList() {
-        if (isEmpty()) {
-            System.out.println("List is empty");
-            return;
-        }
-        Node temp = headNode;
-        System.out.println("List: ");
-        while (temp.nextNode != null) {
-            System.out.print(temp.data + " -> ");
-            temp = temp.nextNode;
-        }
-        System.out.println(temp.data.toString() + " -> null");
-    }
-
-    public void setHeadNode(Node headNode) {
-        this.headNode = headNode;
-    }
-
-    public Node getHeadNode() {
-        return headNode;
-    }
-
-    public int getSize() {
-        return size;
-    }
-
-    public void setSize(int size) {
-        this.size = size;
-    }
-
-    public void removeDuplicateHashing() {
-        Node current = this.headNode;
-        Node prevNode = this.headNode;
-
-        HashSet<T> visitedNodes = new HashSet<>();
-        if (!isEmpty() && current.nextNode != null) {
-            while (current != null) {
-                if (visitedNodes.contains(current.data)) {
-                    prevNode.nextNode = current.nextNode;
-                    current = current.nextNode;
-                } else {
-                    visitedNodes.add(current.data);
-                    prevNode = current;
-                    current = current.nextNode;
-                }
-            }
-        }
-    }
-
-    public static void main(String[] args) {
-        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
-        for (int i = 1; i <= 10; i++) {
-            list.insertAtEnd(i);
-        }
-        list.printList();
-        System.out.println(list.searchNode(8)); // true
-        System.out.println(list.searchNode(11)); // false
-        list.insertAfter(17, 7);
-        list.printList();
-        list.deleteAtHead();
-        list.printList();
-        list.deleteByValue(17);
-        list.printList();
     }
 }
