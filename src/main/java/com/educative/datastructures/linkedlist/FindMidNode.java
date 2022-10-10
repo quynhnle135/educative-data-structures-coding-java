e1a8940fb70d207baf9b8c5b84f663eeb4f513dc
Quynh <quynhnle135@gmail.com>
2022-10-09 21:03:48 -0400

    Find middle node in linkedlist

--- src/main/java/com/educative/datastructures/linkedlist/FindMidNode.java
+++ src/main/java/com/educative/datastructures/linkedlist/FindMidNode.java
@@ -1,60 +1,53 @@
-//package com.educative.datastructures.linkedlist;
-//
-//public class FindMidNode {
-//    public static <T> Object findMidNode(SinglyLinkedList<T> list) {
-//        if (list.isEmpty()) {
-//            return null;
-//        }
-//        SinglyLinkedList<T>.Node mid = list.headNode;
-//        SinglyLinkedList<T>.Node current = list.headNode;
-//        while (mid != null && current != null && current.nextNode != null) {
-//            current = current.nextNode.nextNode;
-//            if (current != null) {
-//                mid = mid.nextNode;
-//            }
-//        }
-//        return mid.data;
-//    }
-//
-//    public static <T> Object findMid(SinglyLinkedList<T> list) {
-//        if (list.isEmpty()) {
-//            return null;
-//        }
-//        int size = list.size;
-//        int pos = -1;
-//        if (size % 2 == 0) {
-//            pos = size / 2 - 1;
-//        } else {
-//            pos = size / 2;
-//        }
-//
-//        SinglyLinkedList<T>.Node current = list.headNode;
-//        while (current != null && pos > 0) {
-//            pos--;
-//            current = current.nextNode;
-//        }
-//        if (current != null) {
-//            return current.data;
-//        }
-//        return -1;
-//    }
-//
-//    public static void main(String[] args) {
-//        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
-//        for (int i = 5; i >= 1; i--) {
-//            list.insertAtHead(i);
-//        }
-//        list.printList();
-//        System.out.println("Middle node of list is: " + findMidNode(list));
-//        System.out.println("Middle node of list is : " + findMid(list));
-//
-//        System.out.println("Another list");
-//        SinglyLinkedList<Integer> myList = new SinglyLinkedList<>();
-//        for (int i = 10; i >= 1; i--) {
-//            list.insertAtHead(i);
-//        }
-//        list.printList();
-//        System.out.println("Middle node of list is: " + findMidNode(list));
-//        System.out.println("Middle node of list is : " + findMid(list));
-//    }
-//}
+package com.educative.datastructures.linkedlist;
+
+
+public class FindMidNode {
+    public static <T> Object findMiddle(SinglyLinkedList<T> list) {
+        if (list.isEmpty()) {
+            return null;
+        }
+        SinglyLinkedList<T>.Node mid = list.headNode;
+        SinglyLinkedList<T>.Node cur = list.headNode;
+        while (mid != null && cur != null && cur.nextNode != null) {
+            cur = cur.nextNode.nextNode;
+            if (cur != null) {
+                mid = mid.nextNode;
+            }
+        }
+        return mid.data;
+    }
+
+    public static <T> Object findMid(SinglyLinkedList<T> list) {
+        if (list.isEmpty()) {
+            return null;
+        }
+        int size = list.size;
+        int pos = 0;
+        if (size % 2 == 0) {
+            pos = size / 2;
+        } else {
+            pos = (size / 2) + 1;
+        }
+        SinglyLinkedList<T>.Node cur = list.headNode;
+        while (cur != null && pos > 1) {
+            cur = cur.nextNode;
+            pos--;
+        }
+        if (cur != null) {
+            return cur.data;
+        } else {
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+        SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+        for (int i = 1; i <= 15; i++) {
+            list.insertAtEnd(i);
+        }
+        list.printList();
+        System.out.println("Size: " + list.size);
+        System.out.println("Mid element: " + findMiddle(list));
+        System.out.println("Mid element: " + findMid(list));
+    }
+}
