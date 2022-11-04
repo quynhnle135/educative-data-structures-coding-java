54a279e1cba427f6352105f9baa9b354547c5748
Quinn Le <quynhnle135@gmail.com>
2022-11-04 11:10:27 -0400

    Solving quiz question with Next Greater Element problem

--- src/main/java/com/educative/datastructures/stackandqueue/NextGreater.java
+++ src/main/java/com/educative/datastructures/stackandqueue/NextGreater.java
@@ -4,9 +4,11 @@ public class NextGreater {
     public static int[] findGreater(int[] arr) {
         int[] res = new int[arr.length];
         Stack<Integer> stack = new Stack<>(arr.length);
+        int popCount = 0;
         for (int i = arr.length - 1; i >=0 ; i--) {
             if (!stack.isEmpty()) {
                 while (!stack.isEmpty() && stack.top() <= arr[i]) {
+                    popCount++;
                     stack.pop();
                 }
             }
@@ -17,11 +19,12 @@ public class NextGreater {
             }
             stack.push(arr[i]);
         }
+        System.out.println(popCount);
         return res;
     }
 
     public static void main(String[] args) {
-        int[] arr = {4, 6, 3, 2, 8, 1, 11};
+        int[] arr = {27, 1, 2, 3, 4, 5, 26, 6, 7, 8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25};
         int[] res = findGreater(arr);
         for (int i : res) {
             System.out.print(i + " ");
