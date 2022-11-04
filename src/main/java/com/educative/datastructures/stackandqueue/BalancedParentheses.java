26891680ca8d8d07bf2d7df7403725f0244a0a34
Quinn Le <quynhnle135@gmail.com>
2022-11-04 12:00:31 -0400

    Balanced Parentheses review

--- src/main/java/com/educative/datastructures/stackandqueue/BalancedParentheses.java
+++ src/main/java/com/educative/datastructures/stackandqueue/BalancedParentheses.java
@@ -2,28 +2,26 @@ package com.educative.datastructures.stackandqueue;
 
 public class BalancedParentheses {
     public static boolean isBalancedParentheses(String parentheses) {
-        Stack<Character> stack = new Stack<>(parentheses.length());
-        for (int i = 0; i < parentheses.length(); i++) {
-            char character = parentheses.charAt(i);
-
-            if (character == '}' || character == ')' || character == ']') {
-                if (stack.isEmpty()) {
-                    return false;
-                } else if ((character == '}' && stack.pop() == '{') || (character == '[' && stack.pop() == ']') || (character == '(' && stack.pop() == ')')) {
-                    return true;
-                }
-            } else {
-                stack.push(character);
-            }
-        }
-        if (!stack.isEmpty()) {
+        if (parentheses.length() % 2 != 0) {
             return false;
         }
+        Stack<Character> stack = new Stack<>(parentheses.length());
+        for (int i = 0; i < parentheses.length(); i++) {
+            char currentChar = parentheses.charAt(i);
+            if (currentChar == ')' || currentChar == ']' || currentChar == '}') {
+                if (stack.isEmpty()) return false;
+                if ((currentChar == ')' && stack.pop() != '(') || (currentChar == '}' && stack.pop() != '{') || (currentChar == '[' && stack.pop() != ']')) {
+                    return false;
+                }
+            }
+            stack.push(currentChar);
+        }
+        if (!stack.isEmpty()) return false;
         return true;
     }
 
     public static void main(String[] args) {
-        String s = "{[()]}}";
+        String s = "{][}()[}[}";
         System.out.println(isBalancedParentheses(s));
     }
 }
