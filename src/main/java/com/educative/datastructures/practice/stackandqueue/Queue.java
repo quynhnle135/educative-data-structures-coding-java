add4bda0d9e7fc0182729cd67f3580651e98e0e2
Quinn Le <quynhnle135@gmail.com>
2022-11-06 16:46:08 -0500

    Stack & Queue Implemenation review

--- src/main/java/com/educative/datastructures/practice/stackandqueue/Queue.java
+++ src/main/java/com/educative/datastructures/practice/stackandqueue/Queue.java
@@ -1,5 +1,56 @@
 package com.educative.datastructures.practice.stackandqueue;
 
 public class Queue <V> {
+    private int maxSize;
+    private int currentSize;
+    private int front;
+    private int back;
+    private V[] array;
 
+    // Constructor
+    @SuppressWarnings("unchecked")
+    public Queue(int maxSize) {
+        this.maxSize = maxSize;
+        this.currentSize = 0;
+        this.front = 0;
+        this.back = -1;
+        array = (V[]) new Object[maxSize];
+    }
+
+    public int getMaxSize() {
+        return maxSize;
+    }
+
+    public int getCurrentSize() {
+        return currentSize;
+    }
+
+    public boolean isEmpty() {
+        return currentSize == 0;
+    }
+
+    public boolean isFull() {
+        return currentSize == maxSize;
+    }
+
+    public void enqueue(V value) {
+        if (isFull()) {
+            System.out.println("Queue is full!");
+            return;
+        }
+        back = (back + 1) % maxSize;
+        array[back] = value;
+        currentSize++;
+    }
+
+    public V dequeue() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty!");
+            return null;
+        }
+        V temp = array[front];
+        front = (front + 1) % maxSize;
+        currentSize--;
+        return temp;
+    }
 }
--- src/main/java/com/educative/datastructures/practice/stackandqueue/Stack.java
+++ src/main/java/com/educative/datastructures/practice/stackandqueue/Stack.java
@@ -1,5 +1,52 @@
 package com.educative.datastructures.practice.stackandqueue;
 
 public class Stack <V>{
+    private int maxSize;
+    private int currentSize;
+    private V[] array;
+    private int top;
+
+    // Constructor
+    @SuppressWarnings("unchecked")
+    public Stack(int maxSize) {
+        this.maxSize = maxSize;
+        this.currentSize = 0;
+        array = (V[]) new Object[maxSize];
+        this.top = -1;
+    }
+    public int getMaxSize() {
+        return maxSize;
+    }
+
+    public int getCurrentSize() {
+        return currentSize;
+    }
+
+    public boolean isEmpty() {
+        return currentSize == 0;
+    }
+
+    public boolean isFull() {
+        return currentSize == maxSize;
+    }
+
+    public void push(V value) {
+        if (isFull()) {
+            System.out.println("Stack is full!");
+            return;
+        }
+        array[++top] = value;
+        currentSize++;
+    }
+
+    public V pop() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty!");
+            return null;
+        }
+        currentSize--;
+        return array[top--];
+    }
+
 
 }
