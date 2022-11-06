7b64f927afa8c2c89d36aa10a92550c3906c51de
Quinn Le <quynhnle135@gmail.com>
2022-11-06 15:33:18 -0500

    Learned about boolean array

---
+++ src/main/java/com/educative/datastructures/graphs/Demo.java
@@ -0,0 +1,10 @@
+package com.educative.datastructures.graphs;
+
+public class Demo {
+    public static void main(String[] args) {
+        boolean[] visited = new boolean[5];
+        for (boolean e : visited) {
+            System.out.println(e);
+        }
+    }
+}
