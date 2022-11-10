0882d34e9e22aa71b0adaab5f90e5543a7315a60
Quinn Le <quynhnle135@gmail.com>
2022-11-10 15:09:18 -0500

    Graph Breadth First Search review

--- src/main/java/com/educative/datastructures/graphs/Main.java
+++ src/main/java/com/educative/datastructures/graphs/Main.java
@@ -2,12 +2,13 @@ package com.educative.datastructures.graphs;
 
 public class Main {
     public static void main(String[] args) {
-        Graph g1 = new Graph(4);
+        Graph g1 = new Graph(5);
         g1.addEdge(0, 1);
         g1.addEdge(0, 2);
-        g1.addEdge(1, 2);
-        g1.addEdge(2, 3);
         g1.addEdge(1, 3);
+        g1.addEdge(1, 4);
+        g1.addEdge(4, 5);
         g1.printGraph();
+
     }
 }
