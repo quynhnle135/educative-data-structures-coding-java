7ec695c9f7a4562d30e3d98a75541e4d24f33d09
Quinn Le <quynhnle135@gmail.com>
2022-12-15 08:33:52 -0500

    Review Stack implementation

---
+++ src/main/java/com/educative/datastructures/review/stack/Stack.java
@@ -0,0 +1,53 @@
+package com.educative.datastructures.review.stack;
+
+// Manually implement Stack
+
+public class Stack<V> {
+    private int maxSize;
+    private int top;
+    private V array[];
+
+    // Constructor
+    @SuppressWarnings("unchecked")
+    public Stack(int maxSize) {
+        this.maxSize = maxSize;
+        this.top = -1;
+        array = (V[]) new Object[maxSize];
+    }
+
+    public int getMaxSize() {
+        return maxSize;
+    }
+
+    public boolean isEmpty() {
+        return  top == -1;
+    }
+
+    public boolean isFull() {
+        return top == maxSize - 1;
+    }
+
+    public V top() {
+        if (isEmpty()) {
+            return null;
+        }
+        return array[top];
+    }
+
+    public void push(V value) {
+        if (isFull()) {
+            System.out.println("Stack is full!!!");
+            return;
+        }
+        array[++top] = value;
+    }
+
+    public V pop() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty!!!");
+            return null;
+        }
+
+        return array[top--];
+    }
+}
